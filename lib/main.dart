cfad53b447d8d5fe604e3b632c1ab04419847081
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2021-04-19 21:18:02 +0545

    implementing custom painter and touchale

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:touchable/touchable.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,107 +14,138 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyCoolPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Container(
+      child: Center(
+        child: CustomPaint(
+          size: Size(200, 200),
+          painter: LinePainter(),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class LinePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = Colors.teal
+      ..strokeWidth = 15;
+
+    Offset center = Offset(size.width / 2, size.height / 2);
+    canvas.drawCircle(center, 100, paint);
+    // Offset start = Offset(0, size.height / 2);
+    // Offset end = Offset(size.width, size.height / 2);
+
+    // canvas.drawLine(start, end, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class MyCoolPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: MyWidget());
+  }
+}
+
+class PaintingController extends ChangeNotifier {
+  Offset circlePosition;
+  Color backgroundColor = Colors.white;
+
+  void moveCircle(Offset newPosition) {
+    circlePosition = newPosition;
+    notifyListeners();
+  }
+
+  void toggleBackGroundColor() {
+    backgroundColor =
+        backgroundColor == Colors.white ? Colors.blueGrey : Colors.white;
+    notifyListeners();
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<PaintingController>(
+      create: (context) => PaintingController(),
+      child: Consumer<PaintingController>(
+        builder: (context, controller, child) => AnimatedContainer(
+          color: controller.backgroundColor,
+          duration: Duration(seconds: 1),
+          child: CanvasTouchDetector(
+            builder: (context) => CustomPaint(painter: MyPainter(context)),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  final BuildContext context;
+  final PaintingController controller;
+
+  MyPainter(this.context)
+      : controller = Provider.of<PaintingController>(context, listen: false);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    TouchyCanvas touchyCanvas = TouchyCanvas(context, canvas);
+    drawEarth(touchyCanvas, size);
+    drawSun(touchyCanvas, size);
+  }
+
+  void drawEarth(TouchyCanvas canvas, Size size) {
+    var blueCircleCenter = controller.circlePosition ??
+        Offset(size.width / 2, size.height / 2 - 100);
+
+    canvas.drawCircle(blueCircleCenter, 60, Paint()..color = Colors.lightGreen,
+        onPanUpdate: (details) {
+      controller.moveCircle(Offset(blueCircleCenter.dx + details.delta.dx * 2,
+          blueCircleCenter.dy + details.delta.dy * 2));
+    });
+    canvas.drawCircle(
+        blueCircleCenter, 30, Paint()..color = Colors.lightBlueAccent,
+        hitTestBehavior: HitTestBehavior.translucent);
+  }
+
+  void drawSun(TouchyCanvas canvas, Size size) {
+    var orangeCircleCenter = Offset(size.width / 2, size.height / 2 + 100);
+    canvas.drawCircle(
+        orangeCircleCenter, 80, Paint()..color = Colors.orangeAccent,
+        onTapDown: (_) {
+      controller.toggleBackGroundColor();
+    });
+    canvas.drawCircle(orangeCircleCenter, 50, Paint()..color = Colors.orange,
+        hitTestBehavior: HitTestBehavior.translucent);
+    canvas.drawCircle(
+        orangeCircleCenter, 30, Paint()..color = Colors.deepOrangeAccent,
+        hitTestBehavior: HitTestBehavior.translucent);
+    canvas.drawCircle(
+        orangeCircleCenter, 20, Paint()..color = Colors.deepOrange,
+        hitTestBehavior: HitTestBehavior.translucent);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
